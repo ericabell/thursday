25797726ea50905e738b7a79d40df05817ae7264
Eric Abell <crucalculus@gmail.com>
2017-05-17 15:38:57 -0500

    Initial commit

---
+++ sort.py
@@ -0,0 +1,9 @@
+def sort_list(list1):
+    """
+    input: list1 consisting of integers
+    output: list1 sorted from smallest to largest
+
+    >>> sort_list([3,2,4,5,1])
+    [1,2,3,4,5]
+
+    """
