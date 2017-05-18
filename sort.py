66c0474493fc8fb5d7cf2e7c18273a03f7a91c8d
Eric Abell <crucalculus@gmail.com>
2017-05-18 10:46:24 -0500

    Basic bubble sort complete

--- sort.py
+++ sort.py
@@ -7,6 +7,16 @@ def sort_list(list1):
     [1,2,3,4,5]
     """
 
+    swapped = True
+    while(swapped):
+        swapped = False
+        for i in range(len(list1)-1):
+            if list1[i] > list1[i+1]:
+                list1[i], list1[i+1] = list1[i+1], list1[i]
+                swapped = True
+    return list1
+
+
 
 
 print( sort_list([3,2,4,5,1]) )
